a55bf206e762e686130271067d3cba58980af365
wj4616 <wj4616@protonmail.com>
2026-04-03 01:43:35 -0700

    chore: add ThresholdDetector and WetProcessor test files to CMake

---
+++ PsycogVST/tests/TestThresholdDetector.cpp
@@ -0,0 +1,10 @@
+/*
+  PsycogVST - ThresholdDetector tests
+  Tests for per-sample EMA RMS threshold detection with cooldown
+*/
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include "../src/modules/ThresholdDetector.h"
+
+// Tests will be added in Task 16
---
+++ PsycogVST/tests/TestWetProcessor.cpp
@@ -0,0 +1,10 @@
+/*
+  PsycogVST - WetProcessor tests
+  Tests for soft-clip, auto-normalize EMA, freeze-aware release
+*/
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include "../src/modules/WetProcessor.h"
+
+// Tests will be added in Task 17
